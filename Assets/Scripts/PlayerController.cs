321e95e870465473fff16d9b0a047de63a7a7844
ColtonDSnook <144720034+ColtonDSnook@users.noreply.github.com>
2023-09-18 14:58:34 -0300

    Roll A Ball Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 
-// Include the namespace required to use Unity UI and Input System
 using UnityEngine.InputSystem;
 using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
-    // Create public variables for player speed, and for the Text UI game objects
     public float speed;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
@@ -18,24 +16,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private int count;
 
-    // At the start of the game..
     void Start()
-    {
-        // Assign the Rigidbody component to our private rb variable
+    { 
         rb = GetComponent<Rigidbody>();
 
-        // Set the count to zero 
         count = 0;
 
         SetCountText();
 
-        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
         winTextObject.SetActive(false);
     }
 
     void FixedUpdate()
     {
-        // Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
@@ -43,15 +37,13 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+     
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
 
-            // Add one to the score variable 'count'
             count = count + 1;
 
-            // Run the 'SetCountText()' function (see below)
             SetCountText();
         }
     }
@@ -70,7 +62,6 @@ public class PlayerController : MonoBehaviour
 
         if (count >= 12)
         {
-            // Set the text value of your 'winText'
             winTextObject.SetActive(true);
         }
     }
