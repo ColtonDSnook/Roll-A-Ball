cd70d93550f9445c713fabb2b221ee446d869937
Snook <144720034+ColtonDSnook@users.noreply.github.com>
2023-10-10 13:58:47 -0300

    Working AI State Machine

---
+++ Assets/Scripts/ChaseState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseState : State
+{
+    public Transform player;
+    public RoamState roamState;
+    public float chaseDistance = 10.0f;
+    [SerializeField]private NavMeshAgent navMeshAgent;
+    public override State RunCurrentState()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= chaseDistance)
+        {
+            UpdateEnemy();
+        }
+        else
+        {
+            navMeshAgent.ResetPath();
+            return roamState;
+        }
+        return this;
+    }
+    private void UpdateEnemy()
+    {
+        navMeshAgent.SetDestination(player.position);
+    }
+}
+
