33d0493af62ec5bcdc285aa07b60d18f51a561e2
Snook <144720034+ColtonDSnook@users.noreply.github.com>
2023-10-27 12:46:38 -0300

    Cutscene animations added

--- Assets/Scripts/CutSceneEnter.cs
+++ Assets/Scripts/CutSceneEnter.cs
@@ -10,20 +10,21 @@ public class CutSceneEnter : MonoBehaviour
     public GameObject cutsceneCam;
     public GameObject Fade;
     public GameObject EffectEnd;
+    public GameObject EffectEndAnim;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         cutsceneCam.SetActive(true);
         Player.SetActive(false);
+        EffectEnd.SetActive(false);
+        EffectEndAnim.SetActive(true);
         StartCoroutine(FinishCut());
     }
 
     IEnumerator FinishCut()
     {
-        yield return new WaitForSeconds(1);
-        EffectEnd.SetActive(false);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         Fade.SetActive(true);
         yield return new WaitForSeconds(1);
         Cursor.lockState = CursorLockMode.None;
