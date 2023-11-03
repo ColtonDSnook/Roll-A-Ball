6d75dd23e221b9e434959281101c5c35f1bb8ff0
Snook <144720034+ColtonDSnook@users.noreply.github.com>
2023-11-03 16:35:05 -0300

    Added mushroom outlines

--- Assets/Scripts/ChaseState.cs
+++ Assets/Scripts/ChaseState.cs
@@ -9,6 +9,15 @@ public class ChaseState : State
     public RoamState roamState;
     public float chaseDistance = 10.0f;
     [SerializeField]private NavMeshAgent navMeshAgent;
+
+    //private IEnumerator Start()
+    //{
+        //yield return new WaitForSeconds(30);
+        //navMeshAgent.ResetPath();
+        //yield return new WaitForSeconds(3);
+        //Debug.Log("grr");
+        //yield return roamState;
+    //}
     public override State RunCurrentState()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
